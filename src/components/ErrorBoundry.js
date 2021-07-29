647be480a92a648af07572e4d25f99f668446319
MMORONTO <t2wycemo@gmail.com>
2021-07-29 09:13:30 -0400

    Create errorBoudry.js

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ErrorBoundry extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false
+        }
+    }
+}
+
+export default ErrorBoundry;
