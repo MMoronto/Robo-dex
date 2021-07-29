a37216f9c355e719274a6219a86b29968f95959e
MMORONTO <t2wycemo@gmail.com>
2021-07-29 05:52:09 -0400

    refactor App.js. destructure robots & searchfield variables in render method

--- src/containers/App.js
+++ src/containers/App.js
@@ -26,11 +26,12 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         }) 
-        if (this.state.robots.length === 0) {
-            return <h1>...Loading...</h1>
+        if (!robots.length) {
+            return <h1>Loading</h1>
         } else {
             return (
                 <div className='tc' >
