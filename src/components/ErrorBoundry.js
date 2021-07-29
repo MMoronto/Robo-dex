0602c8f2c3a691ce0ad6cfc4ff63d5e37c98c9da
MMORONTO <t2wycemo@gmail.com>
2021-07-29 09:22:38 -0400

    Apply ErrorBoundry. wrap ErrorBoundry around CardList component

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -7,6 +7,17 @@ class ErrorBoundry extends Component {
             hasError: false
         }
     }
+
+    componentDidCatch(error, info) {
+        this.setState({ hasError: true })
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>Oooops. That is not good</h1>
+        }
+        return this.props.children
+    }
 }
 
 export default ErrorBoundry;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 // import { robots } from './robots';
 
@@ -37,7 +38,9 @@ class App extends Component {
                     <h1 className='f1'>Robo-Dex</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
-                        <CardList robots={filteredRobots}/>
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots}/>
+                        </ErrorBoundry>
                     </Scroll>
                 </div>   
             );
